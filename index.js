1687d27adc7eb452732ba63410d8c1129b4fad8f
mhakanda <mhamid_93@yahoo.com>
2017-07-14 09:47:09 -0500

    fourth commit

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const port  = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.get('/',(req,res)=>{
-//res.send(' Hello world');
-//var jj = (__dirname + '/public'+'/home.html');
-//res.send(jj)
-//res.render(jj);
-res.send("");
+res.send(' ');
 
 });
 
-app.listen(port,()=>{
-  console.log(`Server is up at port ${port}`);
-});
+app.listen(port)
+
+// app.listen(port,()=>{
+//   console.log(`Server is up at port ${port}`);
+// });
